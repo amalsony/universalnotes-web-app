2e3bea4a5a4599ef04668b49e89c8738376e7cfa
Amal Sony <amalsony5@gmail.com>
2024-02-15 08:30:51 -0500

    added official api

--- next.config.js
+++ next.config.js
@@ -1,14 +1,21 @@
 /** @type {import('next').NextConfig} */
 
-const isDevelopment = process.env.NODE_ENV !== "production";
-const rewritesConfig = isDevelopment
-  ? [
-      {
-        source: "/api/:path*",
-        destination: "http://localhost:8000/:path*",
-      },
-    ]
-  : [];
+// const isDevelopment = process.env.NODE_ENV !== "production";
+// const rewritesConfig = isDevelopment
+//   ? [
+//       {
+//         source: "/api/:path*",
+//         destination: "http://localhost:8000/:path*",
+//       },
+//     ]
+//   : [];
+
+const rewritesConfig = [
+  {
+    source: "/api/:path*",
+    destination: "https://api.universalnotes.org/:path*",
+  },
+];
 
 const nextConfig = {
   rewrites: async () => rewritesConfig,
