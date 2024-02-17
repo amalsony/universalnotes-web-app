140dd8d7e2257f907d040df2e708267bca3bffa6
Amal Sony <amalsony5@gmail.com>
2024-02-16 21:31:12 -0500

    added current backend url

--- next.config.js
+++ next.config.js
@@ -1,19 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 // const isDevelopment = process.env.NODE_ENV !== "production";
-// const rewritesConfig = isDevelopment
-//   ? [
-//       {
-//         source: "/api/:path*",
-//         destination: "http://localhost:8000/:path*",
-//       },
-//     ]
-//   : [];
 
 const rewritesConfig = [
   {
     source: "/api/:path*",
-    destination: "http://localhost:8000/:path*",
+    destination: "https://api.universalnotes.org/:path*",
   },
 ];
 
