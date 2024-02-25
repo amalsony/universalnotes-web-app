8afcd480a37b4089b2b7d7dd57ab6831220abcfb
Amal Sony <amalsony5@gmail.com>
2024-02-25 11:41:18 -0500

    removed login requirement for 'recent notes' tab

--- components/landing/HomeButton.tsx
+++ components/landing/HomeButton.tsx
@@ -1,23 +1,23 @@
-"use client";
+// "use client";
 import Link from "next/link";
 
 // Context
-import { useAuth } from "@/context/AuthContext";
+// import { useAuth } from "@/context/AuthContext";
 
 export default function HomeButton() {
-  const { authLoading } = useAuth();
-  const { userInfo } = useAuth();
+  //   const { authLoading } = useAuth();
+  //   const { userInfo } = useAuth();
 
   return (
     <>
-      {authLoading ? null : userInfo ? (
-        <Link
-          href="/home"
-          className="text-white/90 py-5 px-3 hover:bg-white/10 transition-all duration-300"
-        >
-          Recent Notes
-        </Link>
-      ) : null}
+      {/* {authLoading ? null : userInfo ? ( */}
+      <Link
+        href="/home"
+        className="text-white/90 py-5 px-3 hover:bg-white/10 transition-all duration-300"
+      >
+        Recent Notes
+      </Link>
+      {/* ) : null} */}
     </>
   );
 }
