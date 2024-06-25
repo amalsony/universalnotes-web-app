4e2a4e3252ac1a69e5370feee108eaa2e6e1b000
Amal Sony <amalsony5@gmail.com>
2024-06-25 13:33:36 -0400

    escape apos

--- components/firefox/Firefox.tsx
+++ components/firefox/Firefox.tsx
@@ -57,7 +57,7 @@ export default function Firefox() {
           If this feature is enabled, UniversalNotes will send the url of the
           page you&apos;re visiting to our backend server when you open a new
           page, check if there&apos;s a note available and display it on that
-          page. The url won't be stored.
+          page. The url won&apos;t be stored when checking for a note.
         </p>
         <p className="mt-2 ml-4 text-white">
           Like the extension and this website, our{" "}
